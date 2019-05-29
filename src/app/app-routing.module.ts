4653de3a82d727e4fc5671d5e26ef40c634cebe4
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-29 12:28:01 +0200

    init commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'pdf-viewer', pathMatch: 'full' },
+  { path: 'pdf-viewer', loadChildren: './pdf-viewer/pdf-viewer.module#PdfViewerPageModule' },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
