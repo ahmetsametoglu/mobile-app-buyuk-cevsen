bb7d0f45ddbc051bd9f734285e1abf43ed2b2012
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-30 18:21:34 +0200

    feat: page and zoom subscriptions created

--- src/app/pdf-viewer/pdf-viewer.page.ts
+++ src/app/pdf-viewer/pdf-viewer.page.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { PdfService, IPdfPage } from '../services/pdf.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pdf-viewer',
   templateUrl: './pdf-viewer.page.html',
   styleUrls: ['./pdf-viewer.page.scss'],
 })
-export class PdfViewerPage implements OnInit {
+export class PdfViewerPage implements OnInit, OnDestroy {
   @ViewChild('pdf_viewer') ngPdfViewer;
   pdfViewerHeight = 0;
+
+  pageSubscription: Subscription;
+  zoomSubscription: Subscription;
+
   zoom = 1;
   pdfMarginTop = 0;
   pdfMarginLeft = 0;
   currentPage: IPdfPage;
 
   constructor(private pdfService: PdfService) { }
-
   ngOnInit() {
+    this.pageSubscription = this.pdfService.getCurrentPage().subscribe(page => {
+      if (!!page) {
+        this.currentPage = page;
+        console.log('currentPage:', this.currentPage);
+      }
+    });
+    this.zoomSubscription = this.pdfService.getZoomFactor().subscribe(zoomFactor => {
+      this.zoom = 1 + zoomFactor / 100;
+      this.centralizePdf();
+      console.log('zoom:', this.zoom);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.pageSubscription) {
+      this.pageSubscription.unsubscribe();
+    }
+
+    if (this.zoomSubscription) {
+      this.zoomSubscription.unsubscribe();
+    }
   }
 
   pageRendered() {
@@ -39,4 +64,8 @@ export class PdfViewerPage implements OnInit {
     this.pdfService.scaleZoomFactor(event.scale);
   }
 
+  centralizePdf() {
+    this.pdfMarginLeft = (this.zoom - 1) * -110;
+    this.pdfMarginTop = (this.zoom - 1) * -110;
+  }
 }
