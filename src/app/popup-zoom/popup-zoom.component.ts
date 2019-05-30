0af4b204f7ed281cd4e6e33ffa0afe7105373ccb
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-30 18:46:26 +0200

    feat: zoom component created

---
+++ src/app/popup-zoom/popup-zoom.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { PdfService } from '../services/pdf.service';
+
+@Component({
+  selector: 'app-popup-zoom',
+  templateUrl: './popup-zoom.component.html',
+  styleUrls: ['./popup-zoom.component.scss'],
+})
+export class PopupZoomComponent implements OnInit {
+
+  constructor(private pdfService: PdfService) { }
+  zoomFactor = 1;
+
+  ngOnInit() {
+    this.pdfService.getZoomFactor().pipe(take(1)).toPromise()
+      .then(zoomFactor => {
+        this.zoomFactor = zoomFactor;
+      });
+  }
+
+  onChangeZoom(event: any) {
+    this.zoomFactor = event.detail.value;
+    this.pdfService.setZoomFactor(this.zoomFactor);
+  }
+
+}
