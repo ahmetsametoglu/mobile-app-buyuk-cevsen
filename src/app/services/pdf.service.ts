2d5aefa879856d43734e65e44b4ff703e925f463
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-30 18:05:45 +0200

    feat: pdf page interface created

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import pdfInfo from '../../assets/data/pdf-content.json';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PdfService {
 
-  constructor() {
-    console.log(pdfInfo);
-  }
 }
+
+export interface IPdfPage {
+  pageIndex: number;
+  pageNumber: number;
+  contentTitle: string;
+  description: string;
+  showOnContentMenu: boolean;
+  group: string;
+}
+
