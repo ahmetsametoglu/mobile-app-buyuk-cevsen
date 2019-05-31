4743a67c11b3fbb9409b6297878a3811570fd895
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-31 13:01:53 +0200

    feat: settings menu design cmpleted

--- src/app/components/settings-menu/settings-menu.component.ts
+++ src/app/components/settings-menu/settings-menu.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IPdfPage, PdfService } from 'src/app/services/pdf.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-settings-menu',
   templateUrl: './settings-menu.component.html',
   styleUrls: ['./settings-menu.component.scss'],
 })
-export class SettingsMenuComponent implements OnInit {
+export class SettingsMenuComponent implements OnInit, OnDestroy {
+  viewGroup = 'am';
+  pageSubscription: Subscription;
+  currentPage: IPdfPage;
 
-  constructor() { }
 
-  ngOnInit() {}
+  constructor(private pdfService: PdfService) { }
+
+  ngOnInit() {
+    this.pageSubscription = this.pdfService.getCurrentPage().subscribe(page => {
+      if (!!page) {
+        this.currentPage = page;
+      }
+    });
+
+  }
+
+  ngOnDestroy() {
+    if (this.pageSubscription) {
+      this.pageSubscription.unsubscribe();
+    }
+  }
+
+  changeViewGroup(event) {
+    console.log(event);
+  }
+
+  onNavigateOtherAppPage() {
+
+  }
+
+  showMailSender() {
+
+  }
+
+  closeMenu() {
+
+  }
 
 }
