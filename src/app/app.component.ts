e715dd775c45eb5b6718e08318c2a545c08cc7df
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-05 14:47:00 +0200

    fix: dependancy circulation fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ModalController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 import { AppRateService } from './services/app-rate.service';
+import { AppRateComponent } from './components/app-rate/app-rate.component';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +18,18 @@ export class AppComponent {
     private statusBar: StatusBar,
     private insomnia: Insomnia,
     private appRateService: AppRateService,
+    private modalController: ModalController,
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.appRateService.checkAppRate().then(showModal => {
+        if (showModal) {
+          this.showAppRateModal();
+        }
+      });
       this.appRateService.checkAppRate();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
@@ -32,5 +39,10 @@ export class AppComponent {
           (err) => console.log('keepAwake error:', err));
       }
     });
+  }
+
+  async showAppRateModal() {
+    const modal = await this.modalController.create({ component: AppRateComponent });
+    modal.present();
   }
 }
--- src/app/services/app-rate.service.ts
+++ src/app/services/app-rate.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { ModalController } from '@ionic/angular';
 import { AppRateComponent } from '../components/app-rate/app-rate.component';
 
 @Injectable({
@@ -10,36 +9,31 @@ export class AppRateService {
 
   constructor(
     private storage: Storage,
-    private modalController: ModalController
   ) { }
 
-  checkAppRate() {
-    this.storage.get('show_apprate').then(show => {
-      console.log('show_apprate', show);
-      if (show === null) {
-        show = true;
-        this.storage.set('show_apprate', true);
-      }
+  async checkAppRate() {
+    let show = await this.storage.get('show_apprate');
+    console.log('show_apprate', show);
+    if (show === null) {
+      show = true;
+      this.storage.set('show_apprate', true);
+    }
 
-      if (show) {
-        this.storage.get('rate_countdown').then(countdown => {
-          console.log('countdown:', countdown);
-          if (countdown === null) {
-            countdown = 4;
-          } else if (countdown >= 0) {
-            countdown--;
-          }
-          if (countdown <= 0) {
-            this.showAppRateModal();
-          }
-          this.storage.set('rate_countdown', countdown);
-        });
+    if (show) {
+      let countdown = await this.storage.get('rate_countdown');
+      console.log('countdown:', countdown);
+      if (countdown === null) {
+        countdown = 4;
+      } else if (countdown >= 0) {
+        countdown--;
       }
-    });
-  }
+      this.storage.set('rate_countdown', countdown);
 
-  async showAppRateModal() {
-    let modal = await this.modalController.create({ component: AppRateComponent });
-    modal.present();
+      if (countdown <= 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
