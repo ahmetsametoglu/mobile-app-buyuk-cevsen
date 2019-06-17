2fb69ac73027dd52972e4f38d260f1912f7b6cc9
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-18 00:33:46 +0200

    feat: add bookmark

--- src/app/components/bookmark/bookmark.component.ts
+++ src/app/components/bookmark/bookmark.component.ts
@@ -1,60 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PdfService } from 'src/app/services/pdf.service';
 import { take } from 'rxjs/operators';
 import { AlertController, ToastController } from '@ionic/angular';
+import { BookmarkService } from 'src/app/services/bookmark.service';
+import { IBookmark } from 'src/app/models/bookmark.model';
+import { Subscription } from 'rxjs';
+import { resolve } from 'q';
 @Component({
   selector: 'app-bookmark',
   templateUrl: './bookmark.component.html',
   styleUrls: ['./bookmark.component.scss'],
 })
-export class BookmarkComponent implements OnInit {
+export class BookmarkComponent implements OnInit, OnDestroy {
+
+  bookmarks: IBookmark[] = [];
+  bookmarksSubscription: Subscription;
 
   constructor(
     private pdfService: PdfService,
     public alertController: AlertController,
     public toastController: ToastController,
+    private bookmarkService: BookmarkService,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.bookmarksSubscription = this.bookmarkService.getBookmarks().subscribe(bookmarks => {
+      this.bookmarks = bookmarks;
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.bookmarksSubscription) {
+      this.bookmarksSubscription.unsubscribe();
+    }
+  }
 
   async addBookmark() {
     const currentPage = await this.pdfService.getCurrentPage().pipe(take(1)).toPromise();
     console.log('currentPage:', currentPage);
 
-    const alert = await this.alertController.create({
-      header: `${currentPage.pageNumber}. sayfaya ayraç ekle `,
-      backdropDismiss: false,
-      inputs: [
-        {
-          name: 'name',
-          type: 'text',
-          placeholder: 'ayraça bir isim veriniz'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Iptal',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Confirm Cancel');
-          }
-        }, {
-          text: 'Tamam',
-          handler: (data) => {
-            console.log('Confirm Ok', data);
-            if (data.name.trim() !== '') {
+    const name = await this.showInputAlert(`${currentPage.pageNumber}. sayfaya ayraç ekle `);
+    console.log(name);
+    const bookmark: IBookmark = {
+      id: null,
+      name,
+      pageNumber: currentPage.pageNumber,
+      description: currentPage.description,
+    }
+    this.bookmarkService.saveUpdateBookmark(bookmark);
+  }
 
-            } else {
-              this.showAttention();
+  async showInputAlert(header: string): Promise<string> {
+
+
+    return new Promise(async resolve => {
+      const alert = await this.alertController.create({
+        header: header,
+        backdropDismiss: false,
+        inputs: [
+          {
+            name: 'name',
+            type: 'text',
+            placeholder: 'ayraça bir isim veriniz'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Iptal',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Ekle',
+            handler: (data) => {
+              console.log('Confirm Ok', data);
+              const name = data.name.trim();
+              if (name !== '') {
+                resolve(name)
+              } else {
+                this.showAttention();
+              }
             }
           }
-        }
-      ]
-    });
+        ]
+      });
 
-    await alert.present();
-
+      await alert.present();
+    })
   }
 
   async showAttention() {
