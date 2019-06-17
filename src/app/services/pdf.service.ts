2fecea45f7dd85c671d6e1b9fc94f231c9609e87
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-18 00:51:58 +0200

    style: page reformated feat: setCurrentPageWithPageNumber method created

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import pdfInfo from '../../assets/data/pdf-content.json';
-import { Storage } from '@ionic/storage';
-import { ViewGroupName, NavigationSide, IViewGroup } from '../models/view-group.model.js';
-import { IPdfPage } from '../models/pdfpage.model.js';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import pdfInfo from "../../assets/data/pdf-content.json";
+import { Storage } from "@ionic/storage";
+import {
+  ViewGroupName,
+  NavigationSide,
+  IViewGroup
+} from "../models/view-group.model.js";
+import { IPdfPage } from "../models/pdfpage.model.js";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PdfService {
   private currentPage: BehaviorSubject<IPdfPage> = new BehaviorSubject(null);
   private viewGroup: BehaviorSubject<IViewGroup> = new BehaviorSubject(null);
   private zoomFactor: BehaviorSubject<number> = new BehaviorSubject(0);
   private activePages = [];
-  private pages: BehaviorSubject<IPdfPage[]> = new BehaviorSubject(this.activePages);
+  private pages: BehaviorSubject<IPdfPage[]> = new BehaviorSubject(
+    this.activePages
+  );
 
   constructor(private storage: Storage) {
     this.initStorage();
@@ -26,17 +32,18 @@ export class PdfService {
   }
 
   async initZoomFactor() {
-    const val = await this.storage.get('zoomFactor');
+    const val = await this.storage.get("zoomFactor");
     if (!!val) {
       this.zoomFactor.next(val);
     }
   }
 
   async initLastPage() {
-    const val = await this.storage.get('lastPageNumber');
+    const val = await this.storage.get("lastPageNumber");
     if (!!val) {
-      const lastPage = this.activePages.find(p => p.pageNumber === Number(val))
-        || pdfInfo.pages.find(p => p.pageNumber === Number(val));
+      const lastPage =
+        this.activePages.find(p => p.pageNumber === Number(val)) ||
+        pdfInfo.pages.find(p => p.pageNumber === Number(val));
 
       this.setCurrentPage(lastPage);
     } else {
@@ -46,19 +53,26 @@ export class PdfService {
   }
 
   async initViewGroupAndPages() {
-    let viewGroup = await this.storage.get('viewGroup') as IViewGroup;
+    let viewGroup = (await this.storage.get("viewGroup")) as IViewGroup;
     if (!!viewGroup) {
       this.viewGroup.next(viewGroup);
     } else {
-      viewGroup = { name: ViewGroupName.arapca_meal, navSide: NavigationSide.right } as IViewGroup;
+      viewGroup = {
+        name: ViewGroupName.arapca_meal,
+        navSide: NavigationSide.right
+      } as IViewGroup;
       this.viewGroup.next(viewGroup);
     }
     this.updateActivePages(viewGroup.name);
   }
 
   private updateActivePages(viewGroupName: ViewGroupName) {
-    this.activePages = viewGroupName === ViewGroupName.arapca_meal ? [...pdfInfo.pages]
-      : pdfInfo.pages.filter(p => p.group === viewGroupName).map((p, index) => ({ ...p, pageIndex: index }));
+    this.activePages =
+      viewGroupName === ViewGroupName.arapca_meal
+        ? [...pdfInfo.pages]
+        : pdfInfo.pages
+            .filter(p => p.group === viewGroupName)
+            .map((p, index) => ({ ...p, pageIndex: index }));
 
     this.pages.next(this.activePages);
   }
@@ -83,25 +97,32 @@ export class PdfService {
     }
   }
 
-
   setCurrentPage(v: IPdfPage) {
-    console.log('setCurrentPage', v);
+    console.log("setCurrentPage", v);
 
     if (!v) {
       return;
     }
 
     this.currentPage.next(v);
-    this.storage.set('lastPageNumber', v.pageNumber);
+    this.storage.set("lastPageNumber", v.pageNumber);
+  }
+
+  setCurrentPageWithPageNumber(pageNumber: number) {
+    const page = pdfInfo.pages.find(p => p.pageNumber === pageNumber);
+    if (!!page) {
+      this.setCurrentPage(page);
+    }
   }
 
   setZoomFactor(v: number) {
     this.zoomFactor.next(v);
-    this.storage.set('zoomFactor', v);
+    this.storage.set("zoomFactor", v);
   }
 
   scaleZoomFactor(scale: number) {
-    let newFactor = this.zoomFactor.value + (this.zoomFactor.value * (scale - 1) * 0.2);
+    let newFactor =
+      this.zoomFactor.value + this.zoomFactor.value * (scale - 1) * 0.2;
     newFactor = newFactor < 1 ? 1 : newFactor;
     newFactor = newFactor > 35 ? 35 : newFactor;
     this.setZoomFactor(newFactor);
@@ -133,30 +154,43 @@ export class PdfService {
 
     let viewGroup: IViewGroup = null;
 
-    const viewGroups = await this.storage.get('ViewGroups') as IViewGroup[] || [];
+    const viewGroups =
+      ((await this.storage.get("ViewGroups")) as IViewGroup[]) || [];
     const index = viewGroups.findIndex(v => v.name === viewGroupName);
-    viewGroup = index !== -1 ? viewGroups[index] :
-      { name: viewGroupName, navSide: this.getDefaultNavSide(viewGroupName) };
+    viewGroup =
+      index !== -1
+        ? viewGroups[index]
+        : {
+            name: viewGroupName,
+            navSide: this.getDefaultNavSide(viewGroupName)
+          };
 
-
-    this.storage.set('viewGroup', viewGroup);
-    if (viewGroup.name !== ViewGroupName.arapca_meal && currentPage.group !== ViewGroupName.arapca_meal && viewGroup.name !== currentPage.group) {
+    this.storage.set("viewGroup", viewGroup);
+    if (
+      viewGroup.name !== ViewGroupName.arapca_meal &&
+      currentPage.group !== ViewGroupName.arapca_meal &&
+      viewGroup.name !== currentPage.group
+    ) {
       if (viewGroup.name === ViewGroupName.meal) {
-        this.setCurrentPage(this.activePages[this.currentPage.value.pageIndex + 1]);
+        this.setCurrentPage(
+          this.activePages[this.currentPage.value.pageIndex + 1]
+        );
       } else if (viewGroup.name === ViewGroupName.arapca) {
-        this.setCurrentPage(this.activePages[this.currentPage.value.pageIndex - 1]);
+        this.setCurrentPage(
+          this.activePages[this.currentPage.value.pageIndex - 1]
+        );
       }
     }
     this.viewGroup.next(viewGroup);
     this.updateActivePages(viewGroup.name);
-    this.storage.set('viewGroup', viewGroup);
+    this.storage.set("viewGroup", viewGroup);
     this.updateViewGroups(viewGroup);
   }
 
   saveNavSide(viewGroupName: ViewGroupName, navSide: NavigationSide) {
     const viewGroup = { name: viewGroupName, navSide };
     this.viewGroup.next(viewGroup);
-    this.storage.set('viewGroup', viewGroup);
+    this.storage.set("viewGroup", viewGroup);
     this.updateViewGroups(viewGroup);
   }
 
@@ -164,8 +198,8 @@ export class PdfService {
     if (!viewGroup.name || !viewGroup.navSide) {
       return;
     }
-    this.storage.get('ViewGroups').then((result: IViewGroup[]) => {
-      console.log('ViewGroups:', result);
+    this.storage.get("ViewGroups").then((result: IViewGroup[]) => {
+      console.log("ViewGroups:", result);
 
       if (!result) {
         result = [];
@@ -179,7 +213,7 @@ export class PdfService {
         result[index].navSide = viewGroup.navSide;
       }
 
-      this.storage.set('ViewGroups', result);
+      this.storage.set("ViewGroups", result);
     });
   }
 
@@ -188,21 +222,20 @@ export class PdfService {
 
     switch (viewGroupName) {
       case ViewGroupName.arapca:
-        navSide = NavigationSide.left
+        navSide = NavigationSide.left;
         break;
       case ViewGroupName.meal:
-        navSide = NavigationSide.right
+        navSide = NavigationSide.right;
         break;
       case ViewGroupName.arapca_meal:
-        navSide = NavigationSide.left
+        navSide = NavigationSide.left;
         break;
 
       default:
-        navSide = NavigationSide.right
+        navSide = NavigationSide.right;
         break;
     }
 
     return navSide;
   }
-
 }
