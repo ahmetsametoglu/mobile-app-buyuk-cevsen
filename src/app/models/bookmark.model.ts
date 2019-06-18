970217a2cfd0d2a10f92e7956a935c43fdbf408e
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-18 12:23:27 +0200

    feat: view group added to bookmark

--- src/app/models/bookmark.model.ts
+++ src/app/models/bookmark.model.ts
@@ -1,7 +1,10 @@
+import { IViewGroup } from "./view-group.model";
+
 export interface IBookmark {
-    id: string;
-    name: string;
-    pageNumber: number;
-    description?: string;
-    updatedAt?: Date;
+  id: string;
+  name: string;
+  pageNumber: number;
+  description?: string;
+  updatedAt?: Date;
+  viewGroup: IViewGroup;
 }
