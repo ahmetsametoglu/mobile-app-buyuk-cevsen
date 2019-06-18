f3cb52e8d6041c3d0b76d7ea3362b91cdb15ddc7
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-18 22:05:39 +0200

    feat: exit modal component created

---
+++ src/app/components/exit-modal/exit-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-modal',
+  templateUrl: './exit-modal.component.html',
+  styleUrls: ['./exit-modal.component.scss'],
+})
+export class ExitModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
