4c37d3e4425fb5e170427c8fe3cb7d975ad56043
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-18 22:21:52 +0200

    style: prettier formated

--- src/app/components/app-rate/app-rate.component.ts
+++ src/app/components/app-rate/app-rate.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { AppRateService } from 'src/app/services/app-rate.service';
+import { Component, OnInit } from "@angular/core";
+import { AppRateService } from "src/app/services/app-rate.service";
 
 @Component({
-  selector: 'app-app-rate',
-  templateUrl: './app-rate.component.html',
-  styleUrls: ['./app-rate.component.scss'],
+  selector: "app-app-rate",
+  templateUrl: "./app-rate.component.html",
+  styleUrls: ["./app-rate.component.scss"]
 })
 export class AppRateComponent implements OnInit {
+  constructor(private appRateService: AppRateService) {}
 
-  constructor(
-    private appRateService: AppRateService
-  ) { }
-
-  ngOnInit() { }
+  ngOnInit() {}
 
   onSendMail() {
     this.appRateService.showMailSender();
@@ -20,11 +17,9 @@ export class AppRateComponent implements OnInit {
 
   onAskLater() {
     this.appRateService.askLater();
-
   }
 
   onGoToStore() {
     this.appRateService.goToStore();
   }
-
 }
